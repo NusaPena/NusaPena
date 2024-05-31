6dac043afc4cefd767357943703fea912c20510a
Muhammad Rafi Aria Sarosa <93002344+Rfiraf@users.noreply.github.com>
2024-05-31 19:32:37 +0700

    adding copy webpack plugin
    adding copy webpack plugin

--- webpack.common.js
+++ webpack.common.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
 	entry: {
@@ -26,5 +27,14 @@ module.exports = {
 			filename: "index.html",
 			template: path.resolve(__dirname, "src/templates/index.html"),
 		}),
+
+		new CopyWebpackPlugin({
+			patterns: [
+			  {
+				from: path.resolve(__dirname, "src/public"),
+				to: path.resolve(__dirname, "dist"),
+			  },
+			],
+		  }),
 	],
 };
