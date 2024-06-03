9c68481ff15ac7171cfdb6bbcb6c346b03574ef5
PenulisKode <160392737+IlhamAlifioD@users.noreply.github.com>
2024-06-03 22:01:48 +0700

    Adding routes configuration
    A. Configurate globals for api configuration
    1. Updating api-endpoint.js
    2. Updating congfig.js
    
    B. Configure routing page
    1. Updating routes.js
    
    C. Adding data source
    1. Configure story-source.js for fetch data from API

--- src/scripts/routes/routes.js
+++ src/scripts/routes/routes.js
@@ -1,9 +1,11 @@
 import Tentang from "../views/pages/tentang";
 import Main from "../views/pages/main";
+import ExplorePage from "../views/pages/explore-page";
 
 const routes = {
-    '/': Main,
-    '/tentang': Tentang,
+    "/": Main,
+    "/explore-page": ExplorePage,
+    "/tentang": Tentang,
 };
 
 export default routes;
