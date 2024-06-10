ca526dd91f70e057c183cbda2ee83d72c7b1876e
Muhammad Rafi Aria Sarosa <93002344+Rfiraf@users.noreply.github.com>
2024-06-10 18:31:43 +0700

    adding synopsis, fixing bug
    adding synopsis, fixing bug at detail picture

---
+++ src/scripts/navigation-bar.js
@@ -0,0 +1,37 @@
+class NavigationBar extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    this.render();
+  }
+
+  render() {
+    this.innerHTML = `
+      <div class="nav-title">
+        <a href="#explore" id="skip-link" class="skip-link" aria-label="Jelajahi Cerita">Jelajahi</a>
+        <h2>NusaPena</h2>
+      </div>
+
+      <button id="drawer" class="nav-drawer" aria-label="Navigation Drawer" tabindex="0">☰</button>
+      <nav id="navList" class="nav-list">
+        <ul>
+          <li><a href="#">Beranda</a></li>
+          <li><a href="#/explore">Cerita</a></li>
+          <li><a href="#/favorite">Favorite</a></li>
+          <li><a href="#/about">Tentang Kami</a></li>
+        </ul>
+      </nav>
+    `;
+
+    const drawer = document.querySelector("#drawer");
+    const navList = document.querySelector(".nav-list");
+
+    drawer.addEventListener("click", () => {
+      navList.classList.toggle("open");
+    });
+  }
+}
+
+customElements.define("navigation-bar", NavigationBar);
